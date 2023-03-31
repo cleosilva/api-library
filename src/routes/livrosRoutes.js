9a083f7304d36a6c32a6b08330ba2dae2a87fb35
Cleo <cleo_silva1906@hotmail.com>
2023-03-31 15:03:24 -0300

    Inserindo metodos de inserção de dados no mongo

--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -4,7 +4,8 @@ import livroController from '../controllers/livrosController.js'
 const router = express.Router();
 
 router.get('/livros', livroController.listarLivros);
-router.get('/livros/:id', livroController.listarLivroPorId)
+router.get("/livros/busca", livroController.listarLivrosPorEditora);
+router.get('/livros/:id', livroController.listarLivroPorId);
 router.post('/livros', livroController.cadastrarLivros);
 router.put('/livros/:id', livroController.atualizarLivro);
 router.delete('/livros/:id', livroController.deletarLivro);
